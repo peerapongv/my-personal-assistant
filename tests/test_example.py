683913ce699bb2313ae4024aef79bba80f6323da
Peerapong V <peerapong.van@gmail.com>
2025-05-06 01:37:51 +0700

    Enhance type hinting in test_hello_world function

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,6 +3,6 @@
 from jira_assistant.example import hello_world
 
 
-def test_hello_world():
+def test_hello_world() -> None:
     """Test the hello_world function."""
     assert hello_world() == "Hello, Jira Assistant!"
